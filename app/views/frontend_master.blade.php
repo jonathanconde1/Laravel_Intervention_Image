1d1fdecbba3169eff3cd3808d355ec675667a531
jonathanconde1 <jonathanconde1@gmail.com>
2015-03-05 17:43:42 -0500

    modif

--- app/views/frontend_master.blade.php
+++ app/views/frontend_master.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
 <head>
+
+
 	<meta http-equiv="content-type"
 	content="text/html; charset=utf-8">
 	
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery-1.11.0.min.js"></script>
 	<style>
 		 body {
 			padding-top: 50px;
--- app/views/tpl/album.blade.php
+++ app/views/tpl/album.blade.php
@@ -33,24 +33,26 @@
 		</div>
 	</div>
 	<div class="row">
-	@foreach($album->Photos as $photo)
-		<div class="col-lg-3">
-			<div class="thumbnail" style="max-height: 350px; min-height: 350px;">
-				<img alt="{{$album->name}}" src="/uploads/thumbs/{{$photo->image}}">
-				<div class="caption">
-				<p>{{$photo->description}}</p>
-				
-				<p><p>Created date: {{ date("d F Y",strtotime($photo->created_at)) }} at {{ date("g:ha",strtotime($photo->created_at)) }}
-				</p></p>
-				<a href="{{URL::route('delete_image',array('id'=>$photo->id))}}" 
-					onclick="return confirm('Are you sure?')">
-					<button type="button" class="btn btn-danger btn-small">
-						Delete Image 
-					</button></a>
+		<div class='list-group gallery'>
+		@foreach($album->Photos as $photo)
+			<div class="col-lg-3">
+				<div class="thumbnail" style="max-height: 350px; min-height: 350px;">
+					<img alt="{{$album->name}}" src="/uploads/thumbs/{{$photo->image}}">
+					<div class="caption">
+					<p>{{$photo->description}}</p>
+					
+					<p><p>Created date: {{ date("d F Y",strtotime($photo->created_at)) }} at {{ date("g:ha",strtotime($photo->created_at)) }}
+					</p></p>
+					<a href="{{URL::route('delete_image',array('id'=>$photo->id))}}" 
+						onclick="return confirm('Are you sure?')">
+						<button type="button" class="btn btn-danger btn-small">
+							Delete Image 
+						</button></a>
+					</div>
 				</div>
 			</div>
+		@endforeach
 		</div>
-	@endforeach
 	</div>
 </div>
 @stop
